1c3eb5c99a6c9098825bd841991ef517ef9b8f1f
OGdB <58260499+OGdB@users.noreply.github.com>
2022-03-17 15:07:57 -0400

    Update main.py
    Basis of main

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("Hello")
+import pygame
 
+pygame.init()
+
+win = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+done = False
+while not done:
+    delta_time = clock.tick() / 1000
+
+    # INPUT
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        done = True
+    all_keys = pygame.key.get_pressed()
+    if all_keys[pygame.K_ESCAPE]:
+        done = True
+
+    # DRAWING
+    win.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
