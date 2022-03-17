aa787b5680136c2eb42c05ec7dcf38e563a63844
Stan013 <32732917+Stan013@users.noreply.github.com>
2022-03-17 16:24:45 -0400

    Art

---
+++ Art/Map/tileset forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="tileset forest" tilewidth="16" tileheight="16" tilecount="72" columns="12">
+ <image source="../tileset forest.png" trans="ffffff" width="192" height="96"/>
+</tileset>
