72d058e8ade8382c58adcb9e23e78ecb4108e565
Eozost <davesprite15@gmail.com>
2022-03-17 16:57:14 -0400

    Spear class added
    I added a basic spear class that allows for thrown spears

--- main_tyler.py
+++ main_tyler.py
@@ -1,31 +1,71 @@
 import pygame
 
+class Spear():
+    def __init__(self,player_x,player_y,direction,spear_list):
+        self.player_x = player_x
+        self.player_y = player_y
+        self.position = [player_x,player_y]
+        self.direction = direction
+        self.length = 32
+        self.height = 16
+        self.spear_list = spear_list
+    def make_spear(self):
+        new_spear = [self.position[0],self.position[1],self.direction,self.length,self.height]
+        self.spear_list.append(new_spear)
+
+    def update(self,dt):
+        all_keys = pygame.key.get_pressed()
+        for s in self.spear_list:
+            if s[2] == "left":
+                s[0] -= 5
+            else:
+                s[0] += 5
+        self.position = [self.player_x, self.player_y]
+
+
+    def draw(self,surf):
+        for new_spear in self.spear_list:
+            pygame.draw.rect(surf,(100,100,100),(new_spear[0],new_spear[1],new_spear[3],new_spear[4]))
 
 class Ansgar():
     def __init__(self,player_x,player_y):
         self.position = [player_x,player_y]
+        self.direction = "right"
+        spear_list = []
+
+        self.s = Spear(self.position[0],self.position[1],self.direction,spear_list)
 
     def draw(self, surf):
         pygame.draw.rect(surf,(255,255,0),(self.position[0],self.position[1],64,64))
+        self.s.draw(surf)
 
     def update(self,dt):
         all_keys = pygame.key.get_pressed()
         if all_keys[pygame.K_a] or all_keys[pygame.K_LEFT]:
             if all_keys[pygame.K_LSHIFT]:
-                self.position[0] -= 150 * dt
+                self.position[0] -= 250 * dt #this will let Ansgar run
             else:
-                self.position[0] -= 75 * dt
+                self.position[0] -= 150 * dt
+            self.direction = "left"
         if all_keys[pygame.K_d] or all_keys[pygame.K_RIGHT]:
             if all_keys[pygame.K_LSHIFT]:
-                self.position[0] += 150 * dt
+                self.position[0] += 250 * dt
             else:
-                self.position[0] += 75 * dt
+                self.position[0] += 150 * dt
+            self.direction = "right"
+        if all_keys[pygame.K_SPACE]:
+            self.s.make_spear()
+        self.s.update(dt)
+
+
+
+
 
 pygame.init()
 
 win = pygame.display.set_mode((960, 640),pygame.RESIZABLE)
 clock = pygame.time.Clock()
-a = Ansgar(200,200)
+a = Ansgar(890,580)
 
 done = False
 while not done:
