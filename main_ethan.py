4f6a700200a804e081b048070eb5e36e121f1cc5
Ealxndrk <101667795+Ealxndrk@users.noreply.github.com>
2022-03-22 15:24:33 -0400

    Added border for enemy's movement and moved classes to class folder.

--- main_ethan.py
+++ main_ethan.py
@@ -5,17 +5,15 @@ pygame.init()
 
 win = pygame.display.set_mode((960, 640))
 clock = pygame.time.Clock()
-enemy_group_one = enemy.Enemy(300, 300, 5)
-enemy_group_two = enemy.Enemy(500, 500, 10)
-enemy_group_one.create_enemy()
-enemy_group_two.create_enemy()
+enemy_group_one = enemy.Enemy(300, 300, 5, 100)  # The starting x and y value of the enemy, then how many, then the health
+enemy_group_two = enemy.Enemy(800, 500, 10, 100)
 done = False
 while not done:
     delta_time = clock.tick() / 1000
 
     # UPDATE
-    enemy_group_one.update(delta_time)
-    enemy_group_two.update(delta_time)
+    enemy_group_one.update(delta_time, 300, 900)  # Moves the enemy's with in the given range
+    enemy_group_two.update(delta_time, 100, 600)
 
     # INPUT
     event = pygame.event.poll()
