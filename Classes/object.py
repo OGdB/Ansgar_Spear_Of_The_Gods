025ffe87496b4cb4e0a3af93775b3a43e5907dc3
OGdB <58260499+OGdB@users.noreply.github.com>
2022-03-22 16:28:56 -0400

    More tweaks to the classes.

--- Classes/object.py
+++
@@ -1,9 +0,0 @@
-class Object:
-    """Base class for an object"""
-
-    def __init__(self, start_x, start_y, static):
-        self.x = start_x  # x-Position of object
-        self.y = start_y  # y-Position of object
-
-    def draw(self):
-        pass
