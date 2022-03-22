cd44d06208d847c2678099e93b9641ac0e64f32d
Eozost <davesprite15@gmail.com>
2022-03-22 15:11:36 -0400

    Update main_tyler.py
    I gave Ansgar a rudimentary and janky jump and made the spears shoot left or right depending on what way the player would be facing.

--- main_tyler.py
+++ main_tyler.py
@@ -9,18 +9,40 @@ class Spear():
         self.length = 32
         self.height = 16
         self.spear_list = spear_list
+        self.lifetime = 5
+        self.speed = 275
     def make_spear(self):
-        new_spear = [self.position[0],self.position[1],self.direction,self.length,self.height]
+        new_spear = [self.position[0],self.position[1],self.direction,self.length,self.height,self.lifetime,self.speed]
         self.spear_list.append(new_spear)
 
     def update(self,dt):
         all_keys = pygame.key.get_pressed()
         for s in self.spear_list:
+            s[5] -= dt*1000
             if s[2] == "left":
-                s[0] -= 5
+                s[0] -= s[6] * dt
             else:
-                s[0] += 5
-        self.position = [self.player_x, self.player_y]
+                s[0] += s[6] * dt
+            if s[0] <= 0:
+                s[6] = 0
+        if all_keys[pygame.K_a] or all_keys[pygame.K_LEFT]:
+            self.direction = "left"
+        if all_keys[pygame.K_d] or all_keys[pygame.K_RIGHT]:
+            self.direction = "right"
+
+        if all_keys[pygame.K_a] or all_keys[pygame.K_LEFT]:
+            if all_keys[pygame.K_LSHIFT]:
+                self.position[0] -= 250 * dt  # this will let Ansgar run
+            else:
+                self.position[0] -= 150 * dt
+            self.direction = "left"
+        if all_keys[pygame.K_d] or all_keys[pygame.K_RIGHT]:
+            if all_keys[pygame.K_LSHIFT]:
+                self.position[0] += 250 * dt
+            else:
+                self.position[0] += 150 * dt
+            self.direction = "right"
+
 
 
     def draw(self,surf):
@@ -32,6 +54,10 @@ class Ansgar():
         self.position = [player_x,player_y]
         self.direction = "right"
         spear_list = []
+        self.ansgar_accel = 0
+        self.ansgar_v_speed = 0
+        self.ansgar_max_speed = 2000
+        self.ansgar_d_speed = 0
 
         self.s = Spear(self.position[0],self.position[1],self.direction,spear_list)
 
@@ -39,8 +65,32 @@ class Ansgar():
         pygame.draw.rect(surf,(255,255,0),(self.position[0],self.position[1],64,64))
         self.s.draw(surf)
 
-    def update(self,dt):
+    def update(self,dt,evt):
+
         all_keys = pygame.key.get_pressed()
+
+        if all_keys[pygame.K_w] or all_keys[pygame.K_UP]:
+            self.ansgar_v_speed += 100 * dt
+            self.ansgar_accel += self.ansgar_v_speed
+            self.position[1] -= self.ansgar_accel * dt
+            self.s.position[1] -= self.ansgar_accel * dt
+            if self.ansgar_accel > self.ansgar_max_speed:
+                self.ansgar_accel = -self.ansgar_max_speed
+                self.s.position[1] = -self.ansgar_max_speed
+        else:
+            self.ansgar_d_speed = 10000 * dt
+            self.ansgar_accel = 0
+            self.ansgar_v_speed = 0
+
+                # Decelerate
+
+            if self.position[1] < 580:
+                self.position[1] += self.ansgar_d_speed * delta_time
+                self.s.position[1] += self.ansgar_d_speed * delta_time
+                if self.position[1] > 580:
+                    self.position[1] = 580
+                    self.s.position[1] = 580
+
         if all_keys[pygame.K_a] or all_keys[pygame.K_LEFT]:
             if all_keys[pygame.K_LSHIFT]:
                 self.position[0] -= 250 * dt #this will let Ansgar run
@@ -53,8 +103,12 @@ class Ansgar():
             else:
                 self.position[0] += 150 * dt
             self.direction = "right"
-        if all_keys[pygame.K_SPACE]:
-            self.s.make_spear()
+
+
+
+        if evt.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.s.make_spear()
         self.s.update(dt)
 
 
@@ -81,7 +135,7 @@ while not done:
     if all_keys[pygame.K_ESCAPE]:
         done = True
 
-    a.update(delta_time)
+    a.update(delta_time,event)
 
     # DRAWING
     win.fill((0, 0, 0))
