f8a52ba48f0a11c4d0479e48f49d9019ee21e767
OGdB <58260499+OGdB@users.noreply.github.com>
2022-03-24 15:10:48 -0400

    Create main_pymunk.py
    Demo by Jason added as a point of reference

---
+++ main_pymunk.py
@@ -0,0 +1,60 @@
+import pymunk               # Import pymunk..
+import pygame
+
+pygame.init()
+win_w = 800
+win_h = 600
+win = pygame.display.set_mode((win_w, win_h))
+done = False
+clock = pygame.time.Clock()
+
+
+space = pymunk.Space()      # Create a Space which contain the simulation
+space.gravity = (0,0.0005)      # Set its gravity
+
+
+
+ground_points = ((10, 550), (50, 600), (750, 500))
+
+for i in range(len(ground_points) - 1):
+    seg = pymunk.Segment(space.static_body, ground_points[i], ground_points[i + 1], 0.0)
+    seg.elasticity = 0.95
+    seg.friction = 0.9
+    space.add(seg)
+
+ball_list = []
+
+#poly = pymunk.Poly.create # Create a box shape and attach to body
+#poly.mass = 10              # Set the mass on the shape
+#space.add(body, poly)       # Add both body and shape to the simulation
+
+#print_options = pymunk.SpaceDebugDrawOptions() # For easy printing
+
+while not done:                 # Infinite loop simulation
+    delta_time = clock.tick(60)
+    space.step(delta_time)        # Step the simulation one step forward
+
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        done = False
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            done = False
+    elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        body = pymunk.Body()  # Create a Body
+        body.position = event.pos  # Set the position of the body
+        body_radius = 30
+        body_shape = pymunk.Circle(body, body_radius)
+        body_shape.mass = 10
+        space.add(body, body_shape)
+        ball_list.append(body)
+
+    #space.debug_draw(print_options) # Print the state of the simulation
+    win.fill((0,0,0))
+    for body in ball_list:
+        pygame.draw.circle(win, (255,0,0), body.position, body_radius)
+    for i in range(len(ground_points) - 1):
+        pygame.draw.line(win, (100,100,255), ground_points[i], ground_points[i + 1])
+    pygame.display.flip()
+
+pygame.quit()
