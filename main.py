768741cf4d403768dfad766b327703218065fe53
Eozost <davesprite15@gmail.com>
2022-04-12 16:18:15 -0400

    Minor inconvenience

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ camera_port_h = 1080/4
 start = main_menu.Start_screen(map_size_w, map_size_h)
 start.run()
 
-App = Classes.application.Application(map_size_w, map_size_h, camera_port_w, camera_port_h)
+App = application.Application(map_size_w, map_size_h, camera_port_w, camera_port_h)
 if start.done == True and start.playing == True:
     App.clock.tick()
     App.run()
